c1ee90ba26f6914db28a305c37b9ad74af9ded17
Kayala47 <kjaa2018@mymail.pomona.edu>
2020-04-02 20:39:00 -0700

    fixed pong

--- pong.py
+++ pong.py
@@ -32,42 +32,43 @@ screen = pygame.display.set_mode(size)
 ball = pygame.image.load("ball.png")
 paddle = pygame.image.load("blue-paddle.png")
 
-ballrect = ball.get_rect()
-paddlerect = paddle.get_rect()
+ballRect = ball.get_rect()
+paddleRect = paddle.get_rect()
 
 
 startPos = [width/2,height/2]
-ballrect = ballrect.move(startPos)
+ballRect = ballRect.move(startPos)
 
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
     
-    ballrect = ballrect.move(speed)
-    paddlerect = paddlerect.move(paddleSpeed)
+    ballRect = ballRect.move(speed)
+    paddleRect = paddleRect.move(paddleSpeed)
 
     
     if (paddleRect.bottom >= ballRect.top and paddle.right >= ball.right) or (paddleRect.top <= ball.bottom and paddle.right >= ball.right):
         #misses
+        print("missed")
     
 
-    else if ballrect.right > width:
-        #if the ball hits the paddle or the wall, reverses
+    elif ballRect.right > width:
+        #if the ball hits the the wall, reverses
         speed[0] = -speed[0]
     #every time it hits the right side, scores a point
     
-    else if ballrect.left <= paddlerect.right:
+    elif ballRect.left <= paddleRect.right:
         #if the ball hits the paddle
         speed[0] = -speed[0] #still flip speed
         increase_speed(speed)#increase speed
         print(speed)
 
-    if (ballrect.top < 0 or ballrect.bottom > height):
+    if (ballRect.top < 0 or ballRect.bottom > height):
         #if ball hits top or bottom, goes in reverse
         speed[1] = -speed[1]
 
-    if (paddleRect.bottom >= height) or (paddlerect.top <= 0):
+    if (paddleRect.bottom >= height) or (paddleRect.top <= 0):
         #if paddle touches top or bottom, reverses
         paddleSpeed[1] = -paddleSpeed[1]
 
@@ -84,7 +85,7 @@ while 1:
     #tell the user how many points they have - update the scoreboard
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
-    screen.blit(paddle, paddlerect)
+    screen.blit(ball, ballRect)
+    screen.blit(paddle, paddleRect)
     pygame.display.flip()
 
